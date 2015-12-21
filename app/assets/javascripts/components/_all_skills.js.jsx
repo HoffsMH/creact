6050acc3ec9fd6db25d315ab391a9ab3ccd16ce3
HoffsMH <hecker.matthew@gmail.com>
2015-12-20 18:28:23 -0700

    done adding skill component file

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -2,19 +2,23 @@ var AllSkills = React.createClass({
   handleDelete(id) {
     this.props.handleDelete(id)
   },
+  handleEdit() {
+    console.log('you are in edit!');
+  },
   render() {
     var skills = this.props.skills.map((skill) => {
       return (
         <div key={skill.id}>
-          <h3>{skill.name}</h3>
-          <p><strong>Level:</strong> {skill.level}</p>
-          <p>{skill.details}</p>
-          <button onClick={this.handleDelete.bind(this, skill.id)}>Delete</button>
+        <h3>{skill.name}</h3>
+        <p><strong>Level:</strong> {skill.level}</p>
+        <p>{skill.details}</p>
+        <button onClick={this.handleDelete.bind(this, skill.id)}>Delete</button>
+        <button onClick={this.handleEdit}>Edit</button>
         </div>
       )
     });
     return (
-     <div>{skills}</div>
+      <div>{skills}</div>
     )
   }
 });
