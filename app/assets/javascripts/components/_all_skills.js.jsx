a0c9dd822515aec30fa5da3f7a5162b62299ea84
HoffsMH <hecker.matthew@gmail.com>
2015-12-20 20:15:24 -0700

    done with updating skills

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -2,18 +2,16 @@ var AllSkills = React.createClass({
   handleDelete(id) {
     this.props.handleDelete(id)
   },
-  handleEdit() {
-    console.log('you are in edit!');
+  onUpdate(skill) {
+    this.props.handleUpdate(skill)
   },
   render() {
-    var skills = this.props.skills.map((skill) => {
+    let skills = this.props.skills.map((skill) => {
       return (
         <div key={skill.id}>
-        <h3>{skill.name}</h3>
-        <p><strong>Level:</strong> {skill.level}</p>
-        <p>{skill.details}</p>
-        <button onClick={this.handleDelete.bind(this, skill.id)}>Delete</button>
-        <button onClick={this.handleEdit}>Edit</button>
+        <Skill skill={skill}
+            handleDelete={this.handleDelete.bind(this, skill.id)}
+            handleUpdate={this.onUpdate}/>
         </div>
       )
     });
